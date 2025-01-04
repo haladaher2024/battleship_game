c3003537972e3d294547d1c69a48b8e8e452ce0f
jana <janajafal86@gmail.com>
2025-01-04 19:32:31 +0200

    Update bot.h

--- bot.h
+++ bot.h
@@ -9,8 +9,6 @@ int hits=0;
 int targetCount=0;
 int totalNumberOfShipsSunkByBot = 0;
 int flagShipSunkInCurrentTurn = 0;
-int smokeScreensUsedBot=0;
-int radarSweepsUsedBot=0;
 int   sumheatmap(int num,int type,int** heatmap);
 bool** sunkships(int* ship,char**opponentGrid,char** DisplayedBotGrid);
 char matchingCharacters(int index);
@@ -29,15 +27,10 @@ char updateDisplayedGridBot(char **opponentGrid, char **DisplayGridBot, int next
 int  ShipsSunkByBot(int *ship);
 void torpedo(char **opponentGrid, char **DisplayedBotGrid, int *ship,int ** heatmap);
 char matching(int index);
-int SmokeScreenBot(int **smokeGrid, char **displayedGrid, int shipsSunk, int smokeScreensUsedBot);
 void choosenGrid(char direction, int col, int row, int** heatmap, char** opponentGrid, char** DisplayedBotGrid, int* ship);
 void ArtilleryBot(int**heatmap,int *ship, char**DisplayedBotGrid, char **opponentGrid);
 void torpedoCol(char** DisplayedBotGrid,int columnToApplyTorpedo,char** opponentGrid,int* ship,int**heatmap);
 void torpedoRow(char** DisplayedBotGrid,int rowToApplyTorpedo,char** opponentGrid,int* ship,int** heatmap);
-bool isAdjacentToSunkShip(int row, int col, char **opponentGrid, int *ship);
-int compareCells(const void *a, const void *b, void *heatmap_void);
 void findHighest2x2Grid(int** heatmap, int**Grid);
-void RadarSweepBot(char **opponentGrid, char **displayedGrid, int radarSweepsUsedBot, int **heatmap, int *ship, int **SomkeGridOppd);
-bool isTargetPresent(int row, int col);
-void removeTarget(int index);
+
 #endif
